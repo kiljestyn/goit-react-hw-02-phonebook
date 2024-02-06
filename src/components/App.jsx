9b58446e541479cc4a1d5587ac1d6e9265bab929
kiljestyn <137730997+kiljestyn@users.noreply.github.com>
2024-02-06 21:50:35 +0200

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import css from "./app.module.css";
 import { Component } from "react";
 import { nanoid } from 'nanoid';
 import Phonebook from "./Phonebook/Phonebook";
-import { Filter } from "./Filter/Filter";
+import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -50,7 +55,7 @@ export class App extends Component {
             <ContactList data={filteted} handleDelete={this.handleDelete} />
           </>
         )}
-      </div>
+        </div>
     );
   }
 }
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,37 +1,58 @@
 import css from "./filter.module.css";
+import { Component } from "react";
 
-export const Filter = ({ handleFindChange, state: { filter } }) => {
-    const handleChange = e => {
-      const inputValue = e.target.value;
-  
-      if (!validateName(inputValue)) {
-        alert('Please enter a valid name for filtering');
-        return;
-      }
-  
-      handleFindChange(e);
-    };
-    const handleBackspace = e => {
-      if (e.key === 'Backspace' && filter.length > 0) {
-        const updatedFilter = filter.slice(0, -1);
-        handleFindChange({ target: { value: updatedFilter } });
-      }
-    };
-  
-    const validateName = name => /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/.test(name);
-  
+class Filter extends Component {
+  state = {};
+  render() {
     return (
-      <div className={css.FilterContainer}>
-        <label htmlFor="filter">Find contact by name</label>
-        <input className={css.Input}
+      <div className={css.filterContainer}>
+        <input
           type="text"
-          placeholder="search..."
           name="filter"
-          id="filter"
-          value={filter}
-          onChange={handleChange}
-          onKeyDown={handleBackspace}
+          className={css.input}
+          value={this.props.filter}
+          onChange={this.props.onChange}
+          placeholder="Write name"
         />
       </div>
     );
-  };
+  }
+}
+
+export default Filter;
+
+// export const Filter = ({ handleFindChange, state: { filter } }) => {
+//     const handleChange = e => {
+//       const inputValue = e.target.value;
+  
+//       if (!validateName(inputValue)) {
+//         alert('Please enter a valid name for filtering');
+//         return;
+//       }
+  
+//       handleFindChange(e);
+//     };
+//     const handleBackspace = e => {
+//       if (e.key === 'Backspace' && filter.length > 0) {
+//         const updatedFilter = filter.slice(0, -1);
+//         handleFindChange({ target: { value: updatedFilter } });
+//       }
+//     };
+  
+//     const validateName = name => /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/.test(name);
+  
+//     return (
+//       <div className={css.FilterContainer}>
+//         <label htmlFor="filter">Find contact by name</label>
+//         <input className={css.Input}
+//           type="text"
+//           placeholder="search..."
+//           name="filter"
+//           id="filter"
+//           value={filter}
+//           onChange={handleChange}
+//           onKeyDown={handleBackspace}
+//         />
+//       </div>
+//     );
+//   };
